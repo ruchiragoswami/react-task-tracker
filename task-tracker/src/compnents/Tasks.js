d3bee61706bb959d171b1595ac8c05d106bc073c
ruchiragoswami <ruchigoswami8266@gmail.com>
2022-01-06 16:22:23 -0800

    Deleting a task
    task deleted from UI on click

--- task-tracker/src/compnents/Tasks.js
+++ task-tracker/src/compnents/Tasks.js
@@ -4,12 +4,12 @@ import Task from './Task';
 
 
 
-const Tasks = ({ tasks}) => { 
+const Tasks = ({tasks , onDelete}) => { 
     return (
         <div>
             {tasks.map((task) => ( 
                 // <h3 key={task.id}> {task.test} </h3> 
-                <Task  key={task.id} task={task}/> 
+                <Task  key={task.id} task={task} onDelete={onDelete}/> 
             ))}
         </div>
     )
